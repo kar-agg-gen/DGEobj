3a71b8b6e47a49ce4f87021746563994c3a28085
kar-agg-gen <kartikeya@aggregate-genius.com>
2020-10-15 16:27:08 +0000

    update the test for init.R

--- tests/testthat/test_init.R
+++ tests/testthat/test_init.R
@@ -1,5 +1,44 @@
 context("DGEobj - tests for init.R functions")
 
 
-test_that('', {
+test_that('init.R: initDGEobj', {
+
+    # collect data from test object to initialize new DGEobj
+    counts     <- getItem(DGEobj, "intensity_orig")
+    rowData    <- getItem(DGEobj, "peptideAnnotation_orig")
+    colData    <- getItem(DGEobj, "design_orig")
+    level      <- "gene" # peptide level is not available
+    customAttr <- list(PID       = "20201015-0001",
+                       XpressID  = "12345",
+                       Genome    = "Mouse.B38",
+                       GeneModel = "Ensembl.R84")
+
+    # checking warning as Grange object is not available.
+    expect_warning(test_DgeObj <- initDGEobj(counts     = counts,
+                                             rowData    = rowData,
+                                             colData    = colData,
+                                             level      = level,
+                                             customAttr = customAttr),
+                   "Couldn't build a GRanges object")
+    # verifying class
+    expect_s3_class(test_DgeObj, "DGEobj")
+    expect_type(attributes(test_DgeObj), "list")
+
+    # checking names and dimensions
+    expect_setequal(names(test_DgeObj), c("counts_orig", "counts", "design_orig", "design", "geneData_orig", "geneData" ))
+    expect_equal(dim(test_DgeObj), c(5900, 165))
+
+    # verifying missing value errors
+    expect_error(initDGEobj(rowData =  rowData, colData =  colData, level =  level, customAttr = customAttr),
+                 regexp = "!missing(counts) is not TRUE",
+                 fixed  = TRUE)
+    expect_error(initDGEobj(counts = counts, colData =  colData, level =  level, customAttr = customAttr),
+                 regexp = "!missing(rowData) is not TRUE",
+                 fixed  = TRUE)
+    expect_error(initDGEobj(counts = counts, rowData =  rowData, level =  level, customAttr = customAttr),
+                 regexp = "!missing(colData) is not TRUE",
+                 fixed  = TRUE)
+    expect_error(initDGEobj(counts = counts, rowData =  rowData, colData =  colData, customAttr = customAttr),
+                 regexp = "!missing(level) is not TRUE",
+                 fixed  = TRUE)
 })
