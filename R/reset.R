7075944c26e1958945080e6407bab109baf72b35
kar-agg-gen <kartikeya@aggregate-genius.com>
2020-11-04 10:32:19 +0000

    Updating function and messages

--- R/reset.R
+++ R/reset.R
@@ -28,11 +28,11 @@ resetDGEobj <- function(dgeObj){
     platform.rnaseq <- c("rna-seq", "rnaseq")
 
     assertthat::assert_that("DGEobj" %in% class(dgeObj),
-                            "The DGEobj must be of class 'DGEobj'.")
+                            msg = "The DGEobj must be of class 'DGEobj'.")
     assertthat::assert_that(!is.null(attr(dgeObj, "level")),
-                            "The DGEobj must have a 'level' attribute specified.")
-    assertthat::assert_that(is.null(attr(dgeObj, "PlatformType")),
-                            msg = "Required attribute \"PlatformType\" is missing!  Must use platformType argument.")
+                            msg = "The DGEobj must have a 'level' attribute specified.")
+    assertthat::assert_that(!is.null(attr(dgeObj, "PlatformType")),
+                            msg = "Required attribute \"PlatformType\" is missing.")
 
     if (!is.null(attr(dgeObj, "PlatformType"))) {
         platformType <- tolower(attr(dgeObj, "PlatformType"))
